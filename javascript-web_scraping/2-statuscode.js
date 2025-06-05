8471c404c0814ed3679159e8730c26fca10e6e1e
kennyrukoro <k.rukoro@alustudent.com>
2025-06-05 00:00:38 +0000

    task 2-statuscode.js done

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+if (!url) {
+  console.error('Please provide a URL as the first argument');
+  process.exit(1);
+}
+
+request(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
